653d8441efa10370ebcdf39d892ebc36f8701fdd
imangadydv <angad310501@gmail.com>
2024-09-18 00:27:11 +0530

    add user and video model

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import dotenv from "dotenv"
-
+import express from "express"
 import connectDB from "./db/indesx.js";
+const app = express();
 dotenv.config({
     path:'./env'
 })
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT||8000,()=>{
+        console.log(`server is running ay port:${process.env.PORT}`)
+    })
+})
+.catch((error)=>{
+  console.log("mongodb connection failed",error)
+})
 
 
 
